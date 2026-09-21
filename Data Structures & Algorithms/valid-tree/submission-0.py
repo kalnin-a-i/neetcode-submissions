94ef6ca78d1a5126664feda14ca7ecff24fa0abb
Alexey Kalinin <55480570+kalnin-a-i@users.noreply.github.com>
2026-09-21 19:38:34 +0300

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        graph = [[] for _ in range(n)]
+        for u, v in edges:
+            graph[v].append(u)
+            graph[u].append(v)
+
+        
+        def dfs(v, graph, colors, parent):
+            if colors[v] == 1:
+                return False
+            colors[v] = 1
+            for u in graph[v]:
+                
+                if u == parent:
+                    continue
+                if colors[u] == 1 or not dfs(u, graph, colors, v):
+                    return False
+            
+            colors[v] == 2
+            return True
+
+        colors = [0 for i in range(n)]
+        res = dfs(0, graph, colors, -1)
+        print(res, colors)
+        return res and min(colors) > 0
+            
